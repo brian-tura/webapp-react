380ed9e88b0b1c33fcd389e98ce4fd705fbe3e31
brian-tura <b.tura2002@gmail.com>
2025-06-03 17:16:38 +0200

    [DONE] Milestone 2 done

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
-import { useContext } from 'react'
+import axios from 'axios'
+import { useContext, useEffect, useState } from 'react'
 import MovieContext from '../contexts/MovieContext'
 import { useParams } from 'react-router-dom'
 
 const MoviePage = () => {
-  
-  const { initalMovies } = useContext(MovieContext)
   const { id } = useParams();
-  
+
+  const [movie, setMovie] = useState({})
+  const [reviews, setReviews] = useState([])
+
+  useEffect(() => {
+    axios.get(`http://127.0.0.1:3000/api/movies/${id}`).then((res) => {
+      console.log(res.data)
+      console.log(res.data.reviews)
+      setMovie(res.data)
+      setReviews(res.data.reviews)
+    })
+  }, [])
+
   return (
     <>
       <div className='row'>
@@ -15,8 +26,16 @@ const MoviePage = () => {
           Immagine
         </div>
         <div className="col-12 col-md-6 col-lg-8">
-          <h1>Titolo</h1>
-          <p>Altre informazione</p>
+          <h1>{movie.title}</h1>
+          <p>direttore: {movie.director}</p>
+          <p>genere: {movie.genre}</p>
+          <p>anno: {movie.release_year}</p>
+          {reviews.map((rev) =>
+            <div key={rev.id} className='card mb-3 p-2'>
+              <h4>{rev.name}</h4>
+              <p>{rev.text}</p>
+            </div>
+          )}
         </div>
       </div>
     </>
